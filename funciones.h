0ec5665ed160f89b2de2d10a15c36baf9cd4def7
lesly2002 <50913799+lesly2002@users.noreply.github.com>
2019-11-27 09:48:58 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+typedef size_t range;
+typedef ifstream entrada;
+typedef double decimal;
+typedef size_t positivo;
+typedef void normal;
+typedef string texto;
+vector<texto> lectura(); //Funcion de lectura mal definida, no recomendable trabajar con vector
+typedef char nombre;
+typedef bool estado;
